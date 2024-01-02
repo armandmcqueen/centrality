d3a466a2c76f86d4a28a39c60ac45fbb987a8d70
Armand McQueen <armandmcqueen@gmail.com>
2024-01-01 19:25:04 -0500

    Better wait for the DB to be setup

--- tests/datastore/conftest.py
+++ tests/datastore/conftest.py
@@ -3,6 +3,7 @@ from ..utils.utils import PostgresContainer
 from controlplane.datastore.config import DatastoreConfig
 from controlplane.datastore.client import DatastoreClient
 import time
+from rich import print
 
 
 @pytest.fixture(scope="session")
@@ -11,13 +12,24 @@ def datastore():
     Start the Postgres container, wait for it to be healthy, and clean up after tests are done.
     """
     with PostgresContainer():
-        print("Postgres container launched")
-        SLEEP_TIME = 5
-        print(f"Sleeping for {SLEEP_TIME} seconds to allow postgres to start")
-        time.sleep(SLEEP_TIME)
+        print("⬆ Postgres container launched")
+
         config = DatastoreConfig()
         client = DatastoreClient(config)
-        client.setup_db()
-        print("Setup complete")
+        print("🕕 Waiting for DB to start...")
+
+        start_time = time.time()
+        while True:
+            if time.time() - start_time > 120:
+                raise RuntimeError("❌ Timed out waiting for DB to start")
+            try:
+                client.setup_db()
+                break
+            except Exception as e:
+                print(e)
+                print("🕕 Waiting for DB to start...")
+                time.sleep(1)
+                continue
+        print("✓ DB setup")
 
         yield config, client
